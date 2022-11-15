6e3d925d878caf7ec3018297ef14790fbea69e58
aymurat111 <aymurat111@gmail.com>
2022-11-15 09:21:29 +0100

    com.Cucumber

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,14 +1,16 @@
 package utilities;
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
 public class ConfigReader {
 
     static Properties properties;
 
     static {
         String dosyaYolu="configuration.properties";
+
         try {
             FileInputStream fis = new FileInputStream(dosyaYolu);
             properties=new Properties();
@@ -21,6 +23,7 @@ public class ConfigReader {
     }
 
     public static String getProperty(String key){
+
         return properties.getProperty(key);
     }
 }
