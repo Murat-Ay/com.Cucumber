35258dc1d48ff864cea7cb0b59656cd54a4e5693
aymurat111 <aymurat111@gmail.com>
2023-02-15 17:02:50 +0100

    abc

--- src/test/java/stepdefinitions/MycoursedemyStepdefinitions.java
+++ src/test/java/stepdefinitions/MycoursedemyStepdefinitions.java
@@ -7,6 +7,7 @@ import utilities.ConfigReader;
 
 public class MycoursedemyStepdefinitions {
     MycoursedemyPage mycoursedemyPage=new MycoursedemyPage();
+
     @Then("myCourse anasayfa login linkine tiklar")
     public void my_course_anasayfa_login_linkine_tiklar() {
         mycoursedemyPage.loginLinki.click();
